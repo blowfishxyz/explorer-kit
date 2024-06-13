3ffa9d6cfb6639f7e8749ec1456008c35143db86
CCristi <covali.christian@gmail.com>
2024-06-13 13:49:40 +0200

    feat: Instrument cache hits and misses

--- packages/explorerkit-server/src/core/cache.ts
+++ packages/explorerkit-server/src/core/cache.ts
@@ -1,5 +1,7 @@
+import { Gauge } from "prom-client";
 import { createClient, RedisClientType } from "redis";
 
+import { register } from "@/components/metrics";
 import { config } from "@/core/config";
 import { onTeardown } from "@/utils/teardown";
 
@@ -14,5 +16,43 @@ export async function createCache(): Promise<RedisClientType> {
     await client.disconnect();
   });
 
-  return client as RedisClientType;
+  return instrumentClient(client as RedisClientType);
 }
+
+const instrumentClient = (client: RedisClientType): RedisClientType => {
+  const hitsGauge = new Gauge({
+    name: "cache_hits_total",
+    help: "Total number of cache hits",
+    registers: [register],
+  });
+
+  const missesGauge = new Gauge({
+    name: "cache_misses_total",
+    help: "Total number of cache misses",
+    registers: [register],
+  });
+
+  return new Proxy(client, {
+    get(target, prop, receiver) {
+      if (prop === "get") {
+        return async (key: string) => {
+          const value = await target.get(key);
+
+          if (value) {
+            hitsGauge.inc();
+          } else {
+            missesGauge.inc();
+          }
+
+          return value;
+        };
+      }
+
+      const value = Reflect.get(target, prop, receiver);
+
+      if (typeof value === "function") {
+        return value.bind(target);
+      }
+    },
+  });
+};
