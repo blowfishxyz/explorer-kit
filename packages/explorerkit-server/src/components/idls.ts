92c3051be3323c241636e8265fdb9ed7e00263c9
CCristi <covali.christian@gmail.com>
2024-06-13 10:54:31 +0200

    chore: Simplify load idls

--- packages/explorerkit-server/src/components/idls.ts
+++ packages/explorerkit-server/src/components/idls.ts
@@ -8,35 +8,34 @@ export type IdlsMap = Map<string, SolanaFMParser | null>;
 const IDL_CACHE_TTL = 3600; // one hour
 
 export async function loadAllIdls(programIds: string[]): Promise<IdlsMap> {
+  const idls: IdlsMap = new Map();
   const cache = getSharedDep("cache");
   const cachedIdls = await Promise.allSettled(programIds.map((id) => cache.get(id)));
-  const idls: IdlsMap = new Map();
-
-  const programIdsWithMissingIdls = cachedIdls
-    .map((res, i) => {
-      const programId = programIds[i]!;
-
-      if (res.status === "fulfilled" && res.value) {
-        const idl = deserializeIdl(res.value);
-
-        idls.set(programId, idl && new SolanaFMParser(idl, programId));
-
-        return;
-      }
-
-      return programId;
-    })
-    .filter(Boolean)
-    .map((id) => id!);
 
   await Promise.allSettled(
-    programIdsWithMissingIdls.map(async (programId) => {
-      const idl = await getProgramIdl(programId);
+    cachedIdls
+      .map((res, i) => {
+        const programId = programIds[i]!;
 
-      void cache.set(programId, serializeIdl(idl), { EX: IDL_CACHE_TTL });
+        if (res.status === "fulfilled" && res.value) {
+          const idl = deserializeIdl(res.value);
 
-      idls.set(programId, idl && new SolanaFMParser(idl, programId));
-    })
+          idls.set(programId, idl && new SolanaFMParser(idl, programId));
+
+          return;
+        }
+
+        return programId;
+      })
+      .map(async (programId) => {
+        if (!programId) {
+          return;
+        }
+
+        const idl = await getProgramIdl(programId);
+        void cache.set(programId, serializeIdl(idl), { EX: IDL_CACHE_TTL });
+        idls.set(programId, idl && new SolanaFMParser(idl, programId));
+      })
   );
 
   return idls;
