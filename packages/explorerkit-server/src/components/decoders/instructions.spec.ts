97b802e986ccbf1a5c4e67955d687dae5885c71e
Cristian Covali <christian@phantom.app>
2025-03-13 09:45:16 +0200

    fix: Hardcode OKX and Jup6 idls

--- packages/explorerkit-server/src/components/decoders/instructions.spec.ts
+++ packages/explorerkit-server/src/components/decoders/instructions.spec.ts
@@ -43,7 +43,8 @@ describe("instructions", () => {
   beforeAll(async () => {
     addIdlToRefreshQueue("ComputeBudget111111111111111111111111111111");
     await refreshIdlsInQueue();
-    idls = await loadAllIdls([instruction.programId]);
+    idls = await loadAllIdls([instruction.programId, "JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4"]);
+    console.log(idls);
   });
 
   describe("decodeInstruction", () => {
@@ -78,6 +79,45 @@ describe("instructions", () => {
       encodedData: "3ta97iYzVb3u",
       name: null,
       programId: "UnknownProgramId",
+    });
+  });
+
+  it("should decode properly jupiter route instruction", async () => {
+    const idls = await loadAllIdls(["JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4"]);
+    const result = decodeInstruction(idls, {
+      accountKeys: [
+        "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+        "AnT3fqrEPSH1poYFNXVvcjoSVgttn2HwURUZoWM8FJv8",
+        "7Us6hkutTQCoRQ4tjJrLYrAgCoc8fTnrrLCbHvFhGPcS",
+        "FXTGwxveCKjU96DkZerPtUc8zG2qQqHjj6dd6Q2Tb4CV",
+        "JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4",
+        "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+        "J8gt2jBUi6DW6gdUdm1fkP9oGN4xXwPdQbCZkkN78R3Z",
+        "D8cy77BBepLMngZx6ZukaTff5hCt1HrWyKk3Hnd9oitf",
+        "JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4",
+        "obriQD1zbpyLz95G5n7nJe6a4DPjpFwa5XYPoNm113y",
+        "AvBSC1KmFNceHpD6jyyXBV6gMXFxZ8BJJ3HVUN8kCurJ",
+        "GZsNmWKbqhMYtdSkkvMdEyQF9k5mLmP7tTKYWZjcHVPE",
+        "6YawcNeZ74tRyCv4UfGydYMr7eho7vbUR6ScVffxKAb3",
+        "FpEzVYQ5MjuSut61Ka18tzYhQKLqndefubV7K2U1mrTz",
+        "74tjvZXuW2C7bsBxRYxmwTrqF8BrYr3VDgusj8DwRd9a",
+        "7Us6hkutTQCoRQ4tjJrLYrAgCoc8fTnrrLCbHvFhGPcS",
+        "FXTGwxveCKjU96DkZerPtUc8zG2qQqHjj6dd6Q2Tb4CV",
+        "FpCMFDFGYotvufJ7HrFHsWEiiQCGbkLCtwHiDnh7o28Q",
+        "J4HJYz4p7TRP96WVFky3vh7XryxoFehHjoRySUTeSeXw",
+        "Sysvar1nstructions1111111111111111111111111",
+        "AnT3fqrEPSH1poYFNXVvcjoSVgttn2HwURUZoWM8FJv8",
+        "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
+        "J56q6nX15WHRLJcsGB6s1bjaiywrn8DqLLvLccz61cYx",
+      ],
+      encodedData: "2jtsaD446yyqqK5qJ4chZG7pUNHcDKkCamXAdeGuEmHVMaj1he",
+      programId: "JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4",
+    });
+
+    expect(result.decodedData).toMatchObject({
+      quoted_out_amount: "12260243",
+      slippage_bps: 50,
+      platform_fee_bps: 85,
     });
   });
 });
